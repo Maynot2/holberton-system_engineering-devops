a8b3297ceddef2b2bb623c5a0efa9f6ad5c19f5b
Maynot2 <paulmanot@gmail.com>
2021-02-08 20:24:17 +0000

    Creates 5 zombies

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+  * infinite_while - Runs forever
+  * Return: Nothing
+  */
+
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+/**
+  * main - Where the program starts
+  * Return: Always 0
+  */
+
+int main(void)
+{
+	if (fork() == 0)
+		printf("Zombie process created, PID: %d\n", getpid());
+	else
+		exit(0);
+	if (fork() == 0)
+		printf("Zombie process created, PID: %d\n", getpid());
+	else
+		exit(0);
+	if (fork() == 0)
+		printf("Zombie process created, PID: %d\n", getpid());
+	else
+		exit(0);
+	if (fork() == 0)
+		printf("Zombie process created, PID: %d\n", getpid());
+	else
+		exit(0);
+	if (fork() == 0)
+		printf("Zombie process created, PID: %d\n", getpid());
+	else
+		exit(0);
+	if (fork() == 0)
+		printf("Zombie process created, PID: %d\n", getpid());
+	else
+		exit(0);
+
+	infinite_while();
+
+	return (0);
+}
