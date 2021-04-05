b5f728114c4ed9af5eae2bacd0c76119d0a81e1b
Maynot2 <paulmanot@gmail.com>
2021-04-05 13:03:13 +0000

    Revert back to prev solution

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -14,15 +14,6 @@ file { '/var/www/html/index.html':
   content => 'Holberton School for the win!'
 }
 
-file { '/etc/nginx/sites-available/default':
-    ensure  => present,
-    path    => '/etc/nginx/sites-available/default'
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    content => 'server{listen 80 default_server;listen [::]:80 default_server;root /var/www/html;rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;index index.html index.htm index.nginx-debian.html;server_name _;location / {try_files $uri $uri/ =404;}}'
-}
-
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
