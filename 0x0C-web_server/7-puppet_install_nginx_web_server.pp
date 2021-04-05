cb6e5b328d0efa54ce5c9faf71ddca73af763ee9
Maynot2 <paulmanot@gmail.com>
2021-04-05 10:20:50 +0000

    Add puppet config

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,27 @@
+# puppet manifest to install nginx and some redirects
+
+package { 'nginx':
+  ensure   => installed,
+  provider => 'apt'
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  path    => '/var/www/html/index.html',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  content => 'Holberton School for the win!'
+}
+
+file_line { 'nyan cat rewrite rule':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
