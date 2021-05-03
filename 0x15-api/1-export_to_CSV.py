b9bf2e4ac08e706f5da6d1a74060ac915217d403
Maynot2 <paulmanot@gmail.com>
2021-05-03 10:21:40 +0000

    Remove format function and simplifu logic

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,13 +8,6 @@ import requests
 from sys import argv
 
 
-def format_todos(todos, name):
-    for todo in todos:
-        todo['task'] = todo.pop('title')
-        todo.pop("userId")
-        todo.pop("id")
-        todo['username'] = name
-
 if __name__ == "__main__":
     if len(argv) == 2:
         id_num = argv[1]
@@ -28,13 +21,13 @@ if __name__ == "__main__":
             )
         username = r1.json().get('username')
         todos = r2.json()
-        format_todos(todos, username)
         file_name = '{}.csv'.format(id_num)
-        with open(file_name, "w") as outfile:
+        with open(file_name, 'w') as outfile:
             writer = csv.writer(outfile, quoting=csv.QUOTE_ALL)
             for todo in todos:
                 writer.writerow([
-                        id_num, todo.get('username'),
+                        id_num,
+                        username,
                         todo.get('completed'),
-                        todo.get('task')
+                        todo.get('title')
                     ])
