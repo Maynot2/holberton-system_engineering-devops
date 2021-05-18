55b69ffc2b4c3613e74dd29fe7cb89c47f99b3c3
Maynot2 <paulmanot@gmail.com>
2021-05-18 16:37:11 +0000

    Create script

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+"""
+    Queries the Reddit API
+"""
+
+import requests
+
+
+def count_same_word(text, word):
+    """Counts how many repeated given word in text"""
+    count = 0
+    words = text.strip().split(" ")
+    for w in words:
+        if w.lower() == word:
+            count += 1
+    return count
+
+
+def count_words_rec(subreddit, word_dict, hot_list=[], after=None):
+    """Returns the number hot posts for a given subreddit"""
+
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'APIadvanced/0.0.1'}
+    payload = {'limit': 100, 'after': after}
+    request = requests.get(url, allow_redirects=False, headers=headers,
+                           params=payload)
+
+    if request.status_code == 200:
+        next_page = request.json().get('data').get('after')
+        children = request.json().get('data').get('children')
+        for child in children:
+            title = child.get('data').get('title')
+            for word in word_dict.keys():
+                word_dict[word] += count_same_word(title, word)
+        if next_page:
+            return count_words_rec(subreddit, word_dict, hot_list, next_page)
+        return word_dict
+
+
+def count_words(subreddit, word_list):
+    """Wrapper function for recursive call"""
+    word_dict = {}
+    for word in word_list:
+        word_dict[word] = 0
+    word_dict = count_words_rec(subreddit, word_dict)
+    sorted_list = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
+    for word, count in sorted_list:
+        if count > 0:
+            print('{}: {}'.format(word, count))
