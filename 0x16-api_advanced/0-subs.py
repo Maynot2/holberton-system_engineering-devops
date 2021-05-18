e64fc3005ca8df5560169d49302e2128b6fbbf71
Maynot2 <paulmanot@gmail.com>
2021-05-18 09:38:04 +0000

    Fix pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,11 +11,11 @@ import json
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
 
-    url = 'https://www.reddit.com//r/{}/about.json'.format(subreddit)
-    headers = { 'User-Agent': 'APIadvanced/0.0.1'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'APIadvanced/0.0.1'}
     request = requests.get(url, allow_redirects=False, headers=headers)
 
     if request.status_code == 200:
         return request.json().get('data').get('subscribers')
     else:
-        return 0;
+        return 0
